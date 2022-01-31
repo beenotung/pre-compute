d35d216d85522adccc447a8e30bbcd522fffd875
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-01-31 16:56:41 +0800

    rewrite to inline buffer, and put max in constructor; replace multi target build flow with esbuild;

---
+++ src/browser.ts
@@ -0,0 +1,4 @@
+import { PreCompute } from './pre-compute'
+
+declare const window: any
+window.PreCompute = PreCompute
---
+++ test/new.ts
@@ -0,0 +1,5 @@
+import { PreCompute } from "../src/pre-compute";
+
+let compute = new PreCompute({producer,bufferSize,max})
+compute.get(i)
+compute.prepare(i)
