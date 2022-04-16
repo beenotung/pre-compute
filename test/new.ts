1576b272f690df852b0621d1579b006de65ea63b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-04-17 00:22:32 +0800

    remove unused old files; updated test;

--- test/new.ts
+++
@@ -1,5 +0,0 @@
-import { PreCompute } from "../src/pre-compute";
-
-let compute = new PreCompute({producer,bufferSize,max})
-compute.get(i)
-compute.prepare(i)
--- test/pre-compute-test.ts
+++ test/pre-compute-test.ts
@@ -1,43 +1,28 @@
-import { later } from '@beenotung/tslib/async/wait'
-import { catchMain } from '@beenotung/tslib/node'
 import { PreCompute } from '../src/pre-compute'
 
-function time() {
-  return new Date().toLocaleString()
+let delay = 10
+
+function producer(idx: number) {
+  return new Promise<number>(resolve => setTimeout(() => resolve(idx), delay))
 }
 
-async function fetch(idx: number) {
-  console.log(time(), 'fetch:', idx)
-  await later(500)
-  console.log(time(), 'fetched:', idx)
-  return idx
-}
-
-type Data = number
-
-const n = 20
-const compute = new PreCompute<Promise<Data>>({
+let compute = new PreCompute({
+  producer,
   bufferSize: 5,
-  producer: fetch,
+  max: 10,
 })
 
-async function process(data: Data) {
-  console.log(time(), 'process:', data)
-  await later(50)
-  console.log(time(), 'processed:', data)
-}
-
 async function main() {
-  const start = Date.now()
-  for (let i = 0; i < n; i++) {
-    const data = await compute.get(i, n - 1)
-    // const data = await fetch(i)
-    await process(data)
-    const now = Date.now()
-    console.log('time passed:', now - start, 'ms')
+  let startTime = Date.now()
+  for (let i = 0; i <= 10; i++) {
+    let res = await compute.get(i)
+    console.assert(res === i, 'wrong result from producer')
   }
-  const end = Date.now()
-  console.log('total time used:', end - start, 'ms')
+  let endTime = Date.now()
+  let usedTime = endTime - startTime
+  console.assert(usedTime <= delay * 3, 'producer is not pre-computed')
 }
-
-catchMain(main())
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
